7a76c070648d82b481507075c04e796716fcd28a
Ashkan Pourghasem <ashkan.pourghasem@gmail.com>
2017-10-11 11:35:24 +0330

    Checked responsiveness on index.html

--- js/custom.js
+++ js/custom.js
@@ -3,36 +3,42 @@
 
 	// Page Preloader
 	$(window).load(function() {
+        sameHeight();
 		$(".loader").delay(300).fadeOut();
 		$(".animationload").delay(600).fadeOut("slow");
+        $(window).resize(function () {
+            sameHeight();
+        });
 	});
 
 // Header Aff
 	$("#header-style-1").affix({
 		offset: {
-			top: 100, 
+			top: 100,
 			bottom: function () {
 			return (this.bottom = $('#copyrights').outerHeight(true))
 			}
 		}
 	})
-	
+
 // OWL Carousel
-	$("#owl-testimonial").owlCarousel({
-		items : 1,
-		lazyLoad : true,
-		navigation : false,
-		autoPlay: true
+    $("#owl-testimonial").owlCarousel({
+        items : 1,
+        singleItem: true,
+        lazyLoad : true,
+        navigation : false,
+        autoPlay: true
     });
 
 	$("#owl-testimonial-widget, #owl-blog").owlCarousel({
 		items : 1,
+        singleItem: true,
 		lazyLoad : true,
 		navigation : true,
 		pagination : false,
 		autoPlay: false
     });
-	
+
 	$("#owl_blog_three_line, #owl_portfolio_two_line, #owl_blog_two_line").owlCarousel({
 		items : 2,
 		lazyLoad : true,
@@ -48,7 +54,7 @@
 		pagination : false,
 		autoPlay: true
     });
-	
+
 	$("#services").owlCarousel({
 		items : 3,
 		lazyLoad : true,
@@ -56,7 +62,7 @@
 		pagination : true,
 		autoPlay: true
     });
-	
+
 	$(".buddy_carousel").owlCarousel({
 		items : 9,
 		lazyLoad : true,
@@ -64,12 +70,12 @@
 		pagination : true,
 		autoPlay: true
     });
-	
+
 
 	$('.buddy_tooltip').popover({
 		container: '.buddy_carousel, .buddy_members'
 	});
-		
+
 // Parallax
 	$(window).bind('body', function() {
 		parallaxInit();
@@ -84,31 +90,31 @@
 		$('#seven-parallax').parallax("30%", 0.4);
 	}
 
- 
+
 // Fun Facts
 	function count($this){
 		var current = parseInt($this.html(), 10);
 		current = current + 1; /* Where 50 is increment */
-		
+
 		$this.html(++current);
 			if(current > $this.data('count')){
 				$this.html($this.data('count'));
-			} else {    
+			} else {
 				setTimeout(function(){count($this)}, 50);
 			}
-		}        
-		
+		}
+
 		$(".stat-count").each(function() {
 		  $(this).data('count', parseInt($(this).html(), 10));
 		  $(this).html('0');
 		  count($(this));
 	});
-	
+
 // WOW
 	new WOW(
 		{ offset: 300 }
 	).init();
-		
+
 // DM Top
 	jQuery(window).scroll(function(){
 		if (jQuery(this).scrollTop() > 1) {
@@ -121,7 +127,7 @@
 		jQuery('html, body').animate({scrollTop: '0px'}, 800);
 		return false;
 	});
-	
+
 // Rotate Text
 	$(".rotate").textrotator({
 		animation: "flipUp",
@@ -129,13 +135,13 @@
 	});
 
 
-	
+
 // TOOLTIP
     $('.social-icons, .bs-example-tooltips').tooltip({
       selector: "[data-toggle=tooltip]",
       container: "body"
     })
-	
+
 // Accordion Toggle Items
    var iconOpen = 'fa fa-minus',
         iconClose = 'fa fa-plus';
@@ -149,5 +155,10 @@
           if(e.type == 'hide')
               $(this).find('.accordion-toggle').not($target).removeClass('active');
     });
-	
+
 })(jQuery);
+
+// Same Height Function
+function sameHeight () {
+    $('.same-height').height($('.same-height-parent').height() - 17);
+}
